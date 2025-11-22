2ac9b4765adf76f1a3aeb84e7269c47929a153a8
Rupee88 <chyrupesh828@gmail.com>
2025-11-22 12:05:05 +0545

    added changes

--- components/jobs/JobLocationMap.tsx
+++ components/jobs/JobLocationMap.tsx
@@ -298,7 +298,7 @@ export const JobLocationMap: React.FC<JobLocationMapProps> = ({
             <svg className="w-4 h-4 text-teal-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
             </svg>
-            <span className="text-teal-400">{formatDistance(displayDistance)}</span>
+            <span className="text-teal-400">{formatDistanceDisplay(displayDistance)}</span>
           </div>
         </div>
       )}
